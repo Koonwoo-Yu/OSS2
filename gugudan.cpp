e68ebb74b4326f149aa50f5836ed1eddf8e6ac59
Koonwoo Yu <gunuboard@gmail.com>
2018-04-13 09:58:57 +0900

    add gugudan.cpp

---
+++ gugudan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 8; i++)
+	{
+		cout << "[" << i << "´Ü]" << endl;
+		for (int j = 1; j <= 9; j++)
+		{
+			cout << i << " * " << j << " = " << j * j << endl;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
